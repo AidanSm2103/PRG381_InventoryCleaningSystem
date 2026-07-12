ea0563373dd5c9f4e7f09409c76ffbb97e01866d
AidanSm2103 <aidansm2103@gmail.com>
2026-07-12 13:18:34 +0200

    Created navigation for MainFrame

--- src/inventorycleaning/ui/MainFrame.java
+++ src/inventorycleaning/ui/MainFrame.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-package inventorycleaning.ui;
 
-/**
- *
- * @author spinn
- */
+package inventorycleaning.ui;
+import java.awt.CardLayout;
+import inventorycleaning.util.CardNames;
+
 public class MainFrame extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MainFrame.class.getName());
@@ -17,6 +12,13 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public MainFrame() {
         initComponents();
+        contentPanel.setLayout(new CardLayout());
+    contentPanel.add(new DashboardPanel(), CardNames.DASHBOARD);
+    contentPanel.add(new MaterialsPanel(), CardNames.MATERIALS);
+    contentPanel.add(new SuppliersPanel(), CardNames.SUPPLIERS);
+    contentPanel.add(new CleanersPanel(), CardNames.CLEANERS);
+    contentPanel.add(new IssuancePanel(), CardNames.STOCK_ISSUANCE);
+    contentPanel.add(new ReportsPanel(), CardNames.REPORTS);
     }
 
     /**
@@ -28,21 +30,103 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        navPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        dashboardButton = new javax.swing.JButton();
+        materialsButton = new javax.swing.JButton();
+        suppliersButton = new javax.swing.JButton();
+        cleanersButton = new javax.swing.JButton();
+        issuanceButton = new javax.swing.JButton();
+        reportsButton = new javax.swing.JButton();
+        logoutButton = new javax.swing.JButton();
+        contentPanel = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        navPanel.setBackground(new java.awt.Color(204, 204, 204));
+        navPanel.setLayout(new java.awt.GridLayout(8, 1, 0, 10));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel1.setText("Inventory Cleaning");
+        navPanel.add(jLabel1);
+
+        dashboardButton.setText("Dashboard");
+        dashboardButton.setMinimumSize(new java.awt.Dimension(44, 10));
+        dashboardButton.addActionListener(this::dashboardButtonActionPerformed);
+        navPanel.add(dashboardButton);
+
+        materialsButton.setText("Materials");
+        materialsButton.addActionListener(this::materialsButtonActionPerformed);
+        navPanel.add(materialsButton);
+
+        suppliersButton.setText("Suppliers");
+        suppliersButton.addActionListener(this::suppliersButtonActionPerformed);
+        navPanel.add(suppliersButton);
+
+        cleanersButton.setText("Cleaners");
+        cleanersButton.addActionListener(this::cleanersButtonActionPerformed);
+        navPanel.add(cleanersButton);
+
+        issuanceButton.setText("Issuance");
+        issuanceButton.addActionListener(this::issuanceButtonActionPerformed);
+        navPanel.add(issuanceButton);
+
+        reportsButton.setText("Reports");
+        reportsButton.addActionListener(this::reportsButtonActionPerformed);
+        navPanel.add(reportsButton);
+
+        logoutButton.setText("Logout");
+        navPanel.add(logoutButton);
+
+        getContentPane().add(navPanel, java.awt.BorderLayout.LINE_START);
+
+        contentPanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout contentPanelLayout = new javax.swing.GroupLayout(contentPanel);
+        contentPanel.setLayout(contentPanelLayout);
+        contentPanelLayout.setHorizontalGroup(
+            contentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 683, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        contentPanelLayout.setVerticalGroup(
+            contentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 421, Short.MAX_VALUE)
         );
+
+        getContentPane().add(contentPanel, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void dashboardButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dashboardButtonActionPerformed
+        CardLayout cl = (CardLayout) contentPanel.getLayout();
+        cl.show(contentPanel, CardNames.DASHBOARD);
+    }//GEN-LAST:event_dashboardButtonActionPerformed
+
+    private void materialsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_materialsButtonActionPerformed
+        CardLayout cl = (CardLayout) contentPanel.getLayout();
+        cl.show(contentPanel, CardNames.MATERIALS);
+    }//GEN-LAST:event_materialsButtonActionPerformed
+
+    private void suppliersButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_suppliersButtonActionPerformed
+        CardLayout cl = (CardLayout) contentPanel.getLayout();
+        cl.show(contentPanel, CardNames.SUPPLIERS);
+    }//GEN-LAST:event_suppliersButtonActionPerformed
+
+    private void cleanersButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cleanersButtonActionPerformed
+        CardLayout cl = (CardLayout) contentPanel.getLayout();
+        cl.show(contentPanel, CardNames.CLEANERS);
+    }//GEN-LAST:event_cleanersButtonActionPerformed
+
+    private void issuanceButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_issuanceButtonActionPerformed
+        CardLayout cl = (CardLayout) contentPanel.getLayout();
+        cl.show(contentPanel, CardNames.STOCK_ISSUANCE);
+    }//GEN-LAST:event_issuanceButtonActionPerformed
+
+    private void reportsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reportsButtonActionPerformed
+        CardLayout cl = (CardLayout) contentPanel.getLayout();
+        cl.show(contentPanel, CardNames.REPORTS);
+    }//GEN-LAST:event_reportsButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -70,5 +154,15 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cleanersButton;
+    private javax.swing.JPanel contentPanel;
+    private javax.swing.JButton dashboardButton;
+    private javax.swing.JButton issuanceButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton logoutButton;
+    private javax.swing.JButton materialsButton;
+    private javax.swing.JPanel navPanel;
+    private javax.swing.JButton reportsButton;
+    private javax.swing.JButton suppliersButton;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/inventorycleaning/util/CardNames.java
@@ -0,0 +1,11 @@
+package inventorycleaning.util;
+
+public class CardNames {
+    public static final String DASHBOARD = "DASHBOARD";
+    public static final String MATERIALS = "MATERIALS";
+    public static final String SUPPLIERS = "SUPPLIERS";
+    public static final String CLEANERS = "CLEANERS";
+    public static final String STOCK_ISSUANCE = "STOCK_ISSUANCE";
+    public static final String REPORTS = "REPORTS";
+    
+}
