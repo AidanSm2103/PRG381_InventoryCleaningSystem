4063bcc5e6e3736767ff13d383126cc1f3e26dc0
AidanSm2103 <aidansm2103@gmail.com>
2026-07-13 15:13:20 +0200

    Added SQL Schema for database tables and connected the database

--- src/inventorycleaning/util/DBConnection.java
+++ src/inventorycleaning/util/DBConnection.java
@@ -1,5 +1,19 @@
 package inventorycleaning.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    
+    private static final String URL = "jdbc:derby://localhost:1527/cleaninginventorydb";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientAutoloadedDriver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Derby driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
